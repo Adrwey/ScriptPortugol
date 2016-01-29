1f2a91b580d9d6f2d784b344a68e00807dcf6eb5
Allan <allan.sossego@hotmail.com>
2016-01-29 17:07:10 -0300

    Executável

---
+++ portugol.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+#Instalação do lua
+sudo apt-get install lua5.2
+sudo apt-get install liblua5.2
+
+#Criação do diretório padrão do portugol e instalação luarocks
+
+sudo mkdir /usr/Portugol
+cd /usr/Portugol
+
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+tar zxpf luarocks-2.2.2.tar.gz
+cd luarocks-2.2.2
+./configure; sudo make bootstrap
+sudo luarocks install luasocket
+sudo rm luarocks-2.2.2.tar.gz
+
+sudo luarocks install lpeglabel
+cd /usr/Portugol
+
+#Organização
+
+mkdir lua; cd lua; mkdir include lib
+
+cd include
+
+sudo ln -s /usr/include/lua5.2/lauxlib.h; sudo ln -s
+/usr/include/lua5.2/luaconf.h; sudo  ln -s /usr/include/lua5.2/lua.h;
+sudo ln -s /usr/include/lua5.2/lua.hpp; sudo ln -s/usr/include/lua5.2/lualib.h
+
+cd ../lib
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.2.a liblua.a; sudo ln -s
+/usr/lib/x86_64-linux-gnu/liblua5.2.so liblua.so
+
+
+#PHP-LUA
+
+cd /usr/Portugol
+wget http://pecl.php.net/get/lua-1.1.0.tgz
+tar zxpf lua-1.1.0.tgz
+sudo rm lua-1.1.0.tgz
+cd lua-1.1.0/lua-1.1.0
+
+sudo apt-get install php5-dev
+phpize
+./configure --with-lua=/usr/Portugol/lua
+sudo make
+sudo make install
+
+sudo echo "extension=lua.so" >> /etc/php5/apache2/php.ini 
+cd /etc/php5/mods-available
+sudo touch lua.ini
+echo "extension=lua.so" | sudo tee -a lua.ini
+sudo php5enmod lua
+sudo ln -s /etc/php5/mods-available/lua.ini/etc/php5/cli/conf.d/lua.ini
+
+cd /var/www/html
+sudo wget https://codeload.github.com/Adrwey/Mportugol/zip/master
+sudo unzip master
+sudo rm master 
+
+sudo cp /var/www/html/Mportugol-master/mportugol/portugol-master/*.lua /usr/local/share/lua/5.2 
